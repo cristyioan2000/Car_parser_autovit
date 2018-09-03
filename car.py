f4ebbc25925442ceb65fe767e7ceea088f074bba
cristyioan2000 <33129070+cristyioan2000@users.noreply.github.com>
2018-09-03 23:14:58 +0300

    Update car.py

--- car.py
+++ car.py
@@ -9,7 +9,7 @@ import webbrowser
 import scrapy
 from scrapy.crawler import CrawlerProcess
 start_URLs=[]
-
+page_number_div=""
 
 """
 base_URL = "https://www.autovit.ro/autoturisme/volkswagen/passat/sedan
@@ -27,7 +27,7 @@ https://www.autovit.ro/autoturisme/volkswagen/passat/sedan/de-la-2002
 '''
 https://www.autovit.ro/autoturisme/volkswagen/passat/sedan/de-la-2002/
 '''
-# The url generator with the car specs
+# Generates start url using searching preferences
 class Url_generator():
     def _build_base_url(marca,model,caroserie,an_inceput,pret_maxim,km_maxim,capacitate_cilindrica_maxima):
         base_URL = ""
@@ -36,40 +36,69 @@ class Url_generator():
         base_URL +="&search%5Bfilter_float_engine_capacity%3Ato%5D="+capacitate_cilindrica_maxima+"&search%5Bcountry%5D="
         start_URLs.append(base_URL)
         return start_URLs
-   # def page_number:
-   #def url_gen(marca,moel,caroserie,an_inceput):
-        
-       
-#start_URLs = _build_base_url("volkswagen","passat","sedan","2002","3000","250000","2000")
-urls = Url_generator._build_base_url("volkswagen","passat","sedan","2002","3000","250000","2000")
+    
+urls = Url_generator._build_base_url("volkswagen","passat","sedan","1999","9000","250000","2000")
+# Opens in the broswer the page
 #webbrowser.open(start_URLs[0])
+
+
 
 class CarSpider(scrapy.Spider):
     name = "car"
-    # Link general
+    max_page = ""
+    page_url=""
+    # starting link to parse
     start_urls = [urls[0]]
-    # Sub-link ->postare
-  
-    def parse(self, response ):
+           
+    def parse(self, response ): 
+        page_number_div = response.css('ul.om-pager.rel li a')
+        # the max page for the current search
+        max_page = int(page_number_div.css('span::text').extract()[-1])
+        if max_page>0:
+            for i in range(max_page):
+                if i == (max_page-1):
+                    break
+                page_url=urls[0]+"&page="+str(i+2)                                                    
+                CarSpider.start_urls.append(page_url)
+        # Fetching to parse each page
+        for pages in CarSpider.start_urls:
+          yield response.follow(pages, callback=self.link_parse)
+          # breaking to take just the first page
+          break
+        # Fetching each post from each page
+    def link_parse(self,response):
         for href in response.css('article'):
             link = href.css('div.offer-item__photo a::attr(href)').extract()[0]
-            #base_url = 'https://www.autovit.ro/anunt/volkswagen-passat-ID7Gw6JM.html#b6fd55f20e'
-            yield{
-                    'link' : link
-            }
-            yield response.follow(link, self.parse_post)
+            # printing all the links from the current page
+           # yield{
+            #        'link' : link
+             #       }
+            yield response.follow(link, self.parse_post)                
     def parse_post(self,response):
+        #label_index = 0;
         for post in response.css('div.offer-params'):
             label = post.css('ul.offer-params__list li.offer-params__item span.offer-params__label::text').extract()
             label_value = post.css('ul.offer-params__list li.offer-params__item div.offer-params__value a.offer-params__link::text').extract()
-            to_print = [(l,lv) for l in label for lv in label_value]
-            yield {'attribute': label}                   
-      
+            #for element in label_value:    
+                #element.strip("\n")
             
-            yield{
-                   'label_value' : label_value
+            #to_print = [(l,lv) for l in label for lv in label_value]
+          #  for l in label:
+                # find label index and find the index of the value pair
+                #label_index = index.label if label == l
+               # yield {l: label_value[label_index]] , 
+                #       }                   
+            yield {
+                    'label' : label,
+                     'label_value' : label_value
                     }
-        
+          #  
+           # yield{
+                #   'label_value' : element
+                 #   }
+           # yield {
+            #        'value_len' : len(label)
+             #       }
         
            # label = response.follow.css('ul').extract()
                #div.offer-item__photo a::attr(href)
